a984e23b280ea941bb9babbb55c7997be73a8839
Liam Bard <liam.bard@atritec.se>
2024-11-28 16:46:44 +0100

    api cirrus(int string kvar), börjat med api annotation

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,124 +1,58 @@
 import { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
-// import { Api } from '../data/api.js';
+import { Api } from '../data/api.js';
+// import { annotationInfo } from '../data/annotation.js';
 import loggavit from '../assets/loggavit.png';
 import loggaover from '../assets/loggaover.png';
 import loggagreen from '../assets/loggagreen.png';
 import './Landing.css';
 import { saveAs } from 'file-saver';
 
-  const Landing = ({ trackData }) => {
+const Landing = ({ trackData }) => {
+  const [annotationInputs, setAnnotationInputs] = useState([]);
+  const [webInputs, setWebInputs] = useState([]);
+  const [cirrusInputs, setCirrusInputs] = useState([]);
+  const [isVisible, setIsVisible] = useState(false);
+  const [isSynlig, setIsSynlig] = useState(false);
+  const [isVissa, setIsVissa] = useState(false);
 
-    const [annotationInputs, setAnnotationInputs] = useState([]);
-    const [webInputs, setWebInputs] = useState([]);
-    const [cirrusInputs, setCirrusInputs] = useState([]);
+  const [annotationInfo, setAnnotationInfo] = useState('');
+  const [webInfo, setWebInfo] = useState('');
+  const [cirrusInfo, setCirrusInfo] = useState('');
 
-    const [isVisible, setIsVisible] = useState(false);
-    const [isSynlig, setIsSynlig] = useState(false);
-    const [isVissa, setIsVissa] = useState(false);
-
-    const [annotationInfo, setAnnotationInfo] = useState('');
-    const [webInfo, setWebInfo] = useState('');
-    const [cirrusInfo, setCirrusInfo] = useState('');
-
-    const cirrusParagrams = [
-      { name: 'Ins_freq', type: 'Float', default: 200, desc: "A7 INS Frequency" },
-      { name: 'Path_nas', type: 'String', default: "//192.168.1.6/Mobileye", desc: "Path to NAS / Mobile_mapping drive" },
-      { name: 'move_to_nas', type: 'bBool', default: 5845, desc: "EPSG Code for processing pointcloud data ( pointcloud is exported in this CRS )" },
-      { name: 'clip_input_data', type: 'list', desc:"information for clipping input data. List of [start_time, end_time]." ,default: []},
-      { name: ' generate', desc: "Riegl project Generation options"},
-      { name: 'colorize_scan', type: 'Bool', default: false, desc: "Colorize scan data" },
-      { name: 'export_fast_annotation', type: 'bool', default: false, desc: "Colorize scan data" },
-      { name: 'basic', desc: "Basic operations for all projects"},
-      { name: 'merge_scanners', type: 'Bool', default: false, desc: "Merge left and right scanner" },
-      { name: 'slow_speed_filter', desc: "Slow speed filter" },
-      { name: 'slow_vsz', type: 'Float', default: 0.01, desc: "Voxel Downsample value in meters for slow speed filter" },
-      { name: 'slow_thres_speed', type: 'Float', default: 5.0, package: 'slow_speed_filter', desc: "A7 INS Frequency" },
-      { name: 'point_density', desc: "Density Requirements"},
-      { name: 'step_len_gen_polygons', type: 'Float', default: 2.0, desc: "Length of polygons in meters. Used in computing density."},
-      { name: 'step_len_gen_frames', type: 'Float', default: 0.1, desc: "Length of frames in meters. Used in computing density."},
-      { name: 'min_density', type: 'Float', default: 3000, desc: "Minimum value of density within a polygon"},
-      { name: 'max_density', type: 'Float', default: 5000, desc: "Maximum value of density within a polygon"},
-      { name: 'min_density', type: 'Float', default: 3000, desc: "Minimum Valid Density within a polygon."},
-      { name: 'max_density', type: 'Float', default: 5000, desc: "Maximum Valid Density within a polygon."},
-      { name: 'downsample_vsz',type: 'Float',default: 0.01,desc: "The voxel size in meters for downsampling high density polygons."},
-      { name: 'compute_density_post',type: 'bool', default: false, desc: "Compute the density after downsampling."},
-      { name: 'delivery',desc: "Setup Delivery Folders"},
-      { name: 'grid_size',type: 'int',default: 1000,desc: "Output grid size in meters. 1000 Results in 1km x 1km. 10_000 results in 100m x 100m."},
-      { name: 'group',type: 'int',default: 0,desc: "Group number for delivery folders."},
-      { name: 'lop',type: 'int',default: 1,desc: "Leverans number for delivery folders."},
-      { name: 'op_time_format',type: 'string',default: "stdtime_adjust",choices: 'stdtime_adjust stdtime weeksecs',desc: "Output Time format."},
-      { name: 'epsg_output',type: 'Float',default: 5845,desc: "EPSG Code for delivering pointcloud data."},
-      { name: 'quality_control'},
-      { name: 'sample_laz_grid_step',type: 'int',default: 10000,desc: "Sample length in meters for selecting pointclouds for NH-Data height control. 10_000 meters means every 10km."},
-      { name: 'nhdata_height_threshold',type: 'Float',default: 0.1,desc: "Maximum allowed threshold for a NH-Data height control."},
-    ];
-  
   useEffect(() => {
-      if (!trackData) {
-        return; 
-      }
-
-    const generateInputs = (trackData) => {
-      const inputs = [];
-
-      Object.keys(trackData).forEach((track) => {
-        Object.keys(trackData[track]).forEach((segment) => {
-          const segmentData = trackData[track][segment];
-            const timeRanges = segmentData.time_ranges;
-
-            timeRanges.forEach(([from, to], index) => {
-              inputs.push(
-                { value: '', placeholder: `Track (${track})` },
-                { value: '', placeholder: 'Place_from' },
-                { value: '', placeholder: 'Place_to' },
-                { value: '', placeholder: `Time_ranges_from: ${from}` },
-                { value: '', placeholder: `Time_ranges_to: ${to}` }
-            );
-          });
-        });
-      });
-
-      return inputs;
-    };
-
-    setAnnotationInputs(generateInputs(trackData));
-    setWebInputs(generateInputs(trackData));
-    setCirrusInputs(generateInputs(trackData));
+    if (!trackData) {
+      const cirrusData = Object.entries(Api[0].cirrus.packages).map(([key, packages]) => ({
+        name: key,
+        type: packages.type,
+        value: packages.default,
+        placeholder: packages.desc,
+      }));
+      setCirrusInputs(cirrusData);
+    }
   }, [trackData]);
 
-  useEffect(() => {
-    if (!cirrusParagrams) {
-      return;
-    }
-
-    const generateCirrusInputs = (cirrusParagrams) => {
-      const inputs = [];
-
-      cirrusParagrams.forEach((paragram) => { 
-        if (paragram.type === '') {
-          inputs.push({ value: '', placeholder: paragram.name });
-        } else {
-          inputs.push({ value: '', placeholder: paragram.name });
-        }
-      });
-
-      return inputs;
-    };
-
-    setCirrusInputs(generateCirrusInputs(cirrusParagrams));
-  }, [cirrusParagrams]);
-
-
+  //Annotation Api
+  // useEffect(() => {
+  //   if (!trackData) {
+  //     const annotationData = Object.entries(annotationInfo[0].annotation.packages).map(([key, packages]) => ({
+  //       name: key,
+  //       type: packages.type,
+  //       value: packages.default,
+  //       placeholder: packages.desc,
+  //     }));
+  //     setAnnotationInputs(annotationData);
+  //   }
+  // }, [trackData]);
+   
   const addInput = (section) => {
     if (section === 'annotation') {
       setAnnotationInputs([...annotationInputs, { value: '', placeholder: '' }]);
-    } 
-    else if (section === 'web') {
+    } else if (section === 'web') {
       setWebInputs([...webInputs, { value: '', placeholder: '' }]);
     } else if (section === 'cirrus') {
       setCirrusInputs([...cirrusInputs, { value: '', placeholder: '' }]);
-    } 
+    }
   };
 
   const removeInput = (section) => {
@@ -159,140 +93,139 @@ import { saveAs } from 'file-saver';
   };
 
   const saveToAnnotation = () => {
-    const data = {
-      annotationInputs,
-    };
-
+    const data = { annotationInputs };
     const blob = new Blob([JSON.stringify(data, null, 2)], { type: 'application/json' });
-
     saveAs(blob, 'data.json');
   };
 
   const saveToCirrus = () => {
-    const data = {
-      cirrusInputs
-    };
-
+    const data = { cirrusInputs };
     const blob = new Blob([JSON.stringify(data, null, 2)], { type: 'application/json' });
-
-    saveAs(blob, 'data.json');
+    saveAs(blob, 'data.json');    
   };
 
   const saveToWeb = () => {
-    const data = {
-      webInputs
-    };
-
+    const data = { webInputs };
     const blob = new Blob([JSON.stringify(data, null, 2)], { type: 'application/json' });
-
     saveAs(blob, 'data.json');
-  }; 
+  };
 
   return (
     <>
       <div className="template-container">
-      <div className="Annotation">
-          <h2 onClick={toggleSectionsVisibility}>
-            {isVisible ? 'Annotation' : 'Annotation'}
-          </h2>
+        <div className="Annotation">
+          <h2 onClick={toggleSectionsVisibility}>{isVisible ? 'Annotation' : 'Annotation'}</h2>
         </div>
         <div className="Web360">
-          <h2 onClick={toggleSectionsVissa}>
-            {isVissa ? 'Web360' : 'Web360'}
-          </h2>
+          <h2 onClick={toggleSectionsVissa}>{isVissa ? 'Web360' : 'Web360'}</h2>
         </div>
         <div className="Cirrus">
-          <h2 onClick={toggleSectionsSynlig}>
-            {isSynlig ? 'Cirrus' : 'Cirrus'}
-          </h2>
+          <h2 onClick={toggleSectionsSynlig}>{isSynlig ? 'Cirrus' : 'Cirrus'}</h2>
         </div>
       </div>
 
       <div className="bottom-container">
-    <div className="box">
-      <h2>Activity:</h2>
+        <div className="box">
+          <h2>Activity:</h2>
 
-    {annotationInfo && (
-      <div className="annotation-info">
-        <h2>{annotationInfo}</h2>
-        <div className="Bdl-split">
-          {annotationInputs.map((input, index) => (
-            <input key={index}  type="text"  value={input.value}  placeholder={input.placeholder}  onChange={(e) => {
-              const newInputs = [...annotationInputs];
-                newInputs[index].value = e.target.value;
-                  setAnnotationInputs(newInputs);
-              }}
-            />
-          ))}
-          <div className="button-group">
-              <button className="button-add" onClick={() => addInput('annotation')}>+</button>
-            <button className="button-remove" onClick={() => removeInput('annotation')}>-</button>
-          </div>
-          <button className="save-button" onClick={saveToAnnotation}>Save Annotation</button>
-          <NavLink to="/advance" className="Advance-button">Advance</NavLink>
+          {annotationInfo && (
+            <div className="annotation-info">
+              <h2>{annotationInfo}</h2>
+              <div className="Bdl-split">
+                {annotationInputs.map((input, index) => (
+                  <input
+                    key={index}
+                    type="text"
+                    value={input.value}
+                    placeholder={input.placeholder}
+                    onChange={(e) => {
+                      const newInputs = [...annotationInputs];
+                      newInputs[index].value = e.target.value;
+                      setAnnotationInputs(newInputs);
+                    }}
+                  />
+                ))}
+                <div className="button-group">
+                  <button className="button-add" onClick={() => addInput('annotation')}>+</button>
+                  <button className="button-remove" onClick={() => removeInput('annotation')}>-</button>
+                </div>
+                <button className="save-button" onClick={saveToAnnotation}>Save Annotation</button>
+                <NavLink to="/advance" className="Advance-button">Advance</NavLink>
+              </div>
+            </div>
+          )}
+
+          {webInfo && (
+            <div className="web-info">
+              <h2>{webInfo}</h2>
+              <div className="Bdl-split">
+                {webInputs.map((input, index) => (
+                  <input
+                    key={index}
+                    type="text"
+                    value={input.value}
+                    placeholder={input.placeholder}
+                    onChange={(e) => {
+                      const newInputs = [...webInputs];
+                      newInputs[index].value = e.target.value;
+                      setWebInputs(newInputs);
+                    }}
+                  />
+                ))}
+                <div className="button-group">
+                  <button className="button-add" onClick={() => addInput('web')}>+</button>
+                  <button className="button-remove" onClick={() => removeInput('web')}>-</button>
+                </div>
+                <button className="save-button" onClick={saveToWeb}>Save Web</button>
+                <NavLink to="/advanceWeb" className="AdvanceWeb-button">Advance</NavLink>
+              </div>
+            </div>
+          )}
+          
+          {cirrusInfo && (
+            <div className="cirrus-info">
+              <h2>{cirrusInfo}</h2>
+              <div className="Bdl-split">
+                {cirrusInputs.map((input, index) => (
+                  <input 
+                    key={index} 
+                    type={input.type === 'bool' ? 'checkbox' : 'text'}
+                    value={input.type === 'bool' ? undefined : input.value} placeholder={input.placeholder}
+                    checked={input.type === 'bool' ? input.value : undefined}
+                    onChange={(e) => {
+                      const newInputs = [...cirrusInputs];
+                      if (input.type === 'bool') {
+                        newInputs[index].value = e.target.checked;
+                      } else {
+                        newInputs[index].value = e.target.value;
+                      }
+                      setCirrusInputs(newInputs);
+                    }}
+                  />
+                ))}
+                <div className="button-group">
+                  <button className="button-add" onClick={() => addInput('cirrus')}>+</button>
+                  <button className="button-remove" onClick={() => removeInput('cirrus')}>-</button>
+                </div>
+                <button className="save-button" onClick={saveToCirrus}>Save Cirrus</button>
+                <NavLink to="/advanceCirrus" className="AdvanceCirrus-button">Advance</NavLink>
+              </div>
+            </div>
+          )}
         </div>
       </div>
-    )}
 
-    {webInfo && (
-      <div className="web-info">
-        <h2>{webInfo}</h2>
-        <div className="Bdl-split">
-          {webInputs.map((input, index) => (
-            <input key={index}  type="text"  value={input.value}  placeholder={input.placeholder}  onChange={(e) => {
-                const newInputs = [...webInputs];
-                newInputs[index].value = e.target.value;
-                setWebInputs(newInputs);
-              }}
-            />
-          ))}
-          <div className="button-group">
-            <button className="button-add" onClick={() => addInput('web')}>+</button>
-            <button className="button-remove" onClick={() => removeInput('web')}>-</button>
-          </div>
-          <button className="save-button" onClick={saveToWeb}>Save Web</button>
-          <NavLink to="/advanceWeb" className="AdvanceWeb-button">Advance</NavLink>
+      <div className="sidebar-session">
+        <div className="session-class">
+          <h1>Session</h1>
         </div>
       </div>
-     )}
 
-    {cirrusInfo && (
-      <div className="cirrus-info">
-        <h2>{cirrusInfo}</h2>
-        <div className="Bdl-split">
-          {cirrusInputs.map((input, index) => (
-            <input key={index}  type="text"  value={input.value}  placeholder={input.placeholder}  onChange={(e) => {
-                const newInputs = [...cirrusInputs];
-                newInputs[index].value = e.target.value;
-                setCirrusInputs(newInputs);
-              }}
-            />
-          ))}
-          <div className="button-group">
-            <button className="button-add" onClick={() => addInput('cirrus')}>+</button>
-            <button className="button-remove" onClick={() => removeInput('cirrus')}>-</button>
-          </div>
-          <button className="save-button" onClick={saveToCirrus}>Save Cirrus</button>
-          <NavLink to="/advanceCirrus" className="AdvanceCirrus-button">Advance</NavLink>
-          </div>
-        </div>
-      )}
-    </div>
-    
-    <div className="sidebar-session">
-      <div className="session-class">
-        <h1>Session</h1> 
-        <input type="text" placeholder="Session Name" />
-      </div>
-    </div>
-      
-      <img src={loggagreen} alt="logga" className="logga" />
-      {/* <img src={loggaover} alt="logga" className="logo" />  */}
-      {/* <img src={loggavit} alt="vitlogga" className="vitlogga"  /> */}
-     </div>
-      
-    </>
-  );
+      {/* <img src={loggagreen} alt="logga" className="logga" /> */}
+      {/* <img src={loggaover} alt="logga" className="logga" /> */}
+      <img src={loggavit} alt="logga" className="logga" />
+  </>
+);
 };
 
 export default Landing;
--- src/data/annotation.js
+++ src/data/annotation.js
@@ -1,5 +1,6 @@
-// export const cirrus = [{
-//     "cirrus": {
+// export const annotationInfo = [
+//     {
+// "annotation": {
 //         "params": {
 //             "ins_freq": {
 //                 "type": "float",
@@ -41,14 +42,6 @@
 //                     "desc": "Colorize Scan Data"
 //                 }
 //             },
-//             "basic": {
-//                 "desc": "Basic operations for all projects",
-//                 "merge_scanners": {
-//                     "type": "bool",
-//                     "default": true,
-//                     "desc": "Merge left and right scanner"
-//                 }
-//             },
 //             "slow_speed_filter": {
 //                 "desc": "Slow Speed filtering",
 //                 "slow_vsz": {
@@ -69,59 +62,11 @@
 //                     "desc": "Threshold for selecting time ranges. 15.0 means only select ranges more than equal to 15 seconds."
 //                 }
 //             },
-//             "point_density": {
-//                 "desc": "Density Requirements",
-//                 "step_len_gen_polygons": {
-//                     "type": "float",
-//                     "default": 2,
-//                     "desc": "Length of polygons in meters. Used in computing density."
-//                 },
-//                 "step_len_gen_frames": {
-//                     "type": "float",
-//                     "default": 0.1,
-//                     "desc": "Length of a frame in meters. Used in computing density."
-//                 },
-//                 "min_density": {
-//                     "type": "float",
-//                     "default": 3000,
-//                     "desc": "Minimum Valid Density within a polygon."
-//                 },
-//                 "max_density": {
-//                     "type": "float",
-//                     "default": 5000,
-//                     "desc": "Maximum Valid Density within a polygon."
-//                 },
-//                 "downsample_vsz": {
-//                     "type": "float",
-//                     "default": 0.01,
-//                     "desc": "The voxel size in meters for downsampling high density polygons."
-//                 },
-//                 "compute_density_post": {
-//                     "type": "float",
-//                     "default": false,
-//                     "desc": "Compute the density after downsampling."
-//                 }
-//             },
 //             "delivery": {
 //                 "desc": "Setup Delivery Folders",
-//                 "grid_size": {
-//                     "type": "int",
-//                     "default": 1000,
-//                     "desc": "Output grid size in meters. 1000 Results in 1km x 1km. 10_000 results in 100m x 100m"
-//                 },
-//                 "group": {
-//                     "type": "int",
-//                     "default": 0,
-//                     "desc": "Group number for delivery folders"
-//                 },
-//                 "lop": {
-//                     "type": "int",
-//                     "default": 1,
-//                     "desc": "Leverans number for delivery folders"
-//                 },
 //                 "op_time_format": {
 //                     "type": "string",
-//                     "default": "stdtime_adjust",
+//                     "default": "weeksecs",
 //                     "choices": [
 //                         "stdtime_adjust",
 //                         "stdtime",
@@ -134,19 +79,8 @@
 //                     "default": 5845,
 //                     "desc": "EPSG Code for delivering pointcloud data"
 //                 }
-//             },
-//             "quality_control": {
-//                 "sample_laz_grid_step": {
-//                     "type": "int",
-//                     "default": 10000,
-//                     "desc": "Sample length in meters for selecting pointclouds for NH-Data height control. 10_000 meters means every 10km."
-//                 },
-//                 "nhdata_height_threshold": {
-//                     "type": "float",
-//                     "default": 0.1,
-//                     "desc": "Maximum allowed threshold for a NH-Data height control."
-//                 }
 //             }
 //         }
 //     }
-// }]
+// }
+//     ]
